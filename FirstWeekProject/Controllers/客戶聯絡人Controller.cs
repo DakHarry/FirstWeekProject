6bee16aaab1831fc7cfe33d213bf4bb1273a8366
DakHarry <monkey030210@gmail.com>
2016-10-20 23:24:35 +0800

    Customer Built

---
+++ FirstWeekProject/Controllers/客戶聯絡人Controller.cs
@@ -0,0 +1,24 @@
+﻿using FirstWeekProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWeekProject.Controllers
+{
+    public class 客戶聯絡人Controller : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+        // GET: 客戶聯絡人
+        public ActionResult Index()
+        {          
+            return View(db.客戶聯絡人.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
