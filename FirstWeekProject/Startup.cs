e9007976c574f86ddc9dd0454dfcc95655ad8b98
DakHarry <monkey030210@gmail.com>
2016-10-20 21:02:14 +0800

    加入專案檔案。

---
+++ FirstWeekProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstWeekProject.Startup))]
+namespace FirstWeekProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
